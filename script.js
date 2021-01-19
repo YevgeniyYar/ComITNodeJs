20d8cb63d28e856c10173f5e9e9df207f81c71d7
YevgeniyYar <evgeniy.yaroslavtsev@yahoo.com>
2021-01-19 16:23:56 -0700

    Add modified files

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var button = document.querySelector('button');
+  
+    button.addEventListener('click', function() {
+      alert("We are going to Rock!");
+    });
+  }
